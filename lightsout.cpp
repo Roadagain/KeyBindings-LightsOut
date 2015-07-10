09cd4edab32e06692ae9f00e3424e26e907dd17f
Roadagain <ringoh72@gmail.com>
2015-07-10 15:01:40 +0900

    Init lights of board in constructor

--- lightsout.cpp
+++ lightsout.cpp
@@ -1,4 +1,6 @@
 #include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "lightsout.hpp"
 
@@ -7,9 +9,15 @@ namespace roadagain
 
 board::board(int width, int height) : width(width), height(height)
 {
+    // init seed of rand() by current time
+    srand(time(NULL));
+
     lights = new bool*[height + 2]();
     for (int i = 0; i < height + 2; i++){
         lights[i] = new bool[width + 2]();
+        for (int j = 1; j < width + 1; j++){
+            lights[i][j] = (rand() % 2 == 0);
+        }
     }
 }
 
