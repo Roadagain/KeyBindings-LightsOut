8aba49bcb62249562df25d5fa09df68615fafd87
Roadagain <ringoh72@gmail.com>
2015-07-10 19:01:17 +0900

    Modify main.cpp to make board

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <ncurses.h>
 
+#include "print.hpp"
+#include "lightsout.hpp"
+using namespace roadagain;
+
 int main()
 {
     initscr();
     cbreak();
+
+    board b(5, 5);
+    getch();
 
     endwin();
     return 0;
