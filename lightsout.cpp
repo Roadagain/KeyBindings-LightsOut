f785d7bae745c797d92007aa3cc2157f9ddf83ac
Roadagain <ringoh72@gmail.com>
2015-07-12 00:29:59 +0900

    Change exit condition

--- lightsout.cpp
+++ lightsout.cpp
@@ -39,7 +39,7 @@ bool board::is_perfect()
 {
     for (int i = 1; i <= this->height + 1; i++){
         for (int j = 1; j <= this->width + 1; j++){
-            if (!this->lights[i][j]){
+            if (this->lights[i][j]){
                 return false;
             }
         }
