43fa95abfb795b3581e2db353c321a106e38b5be
Roadagain <ringoh72@gmail.com>
2015-07-15 10:07:17 +0900

    Change creating-board algorithm
    From perfectly randomize to randomized turn, so add turn(y, x, print)

--- lightsout.cpp
+++ lightsout.cpp
@@ -10,7 +10,7 @@ namespace roadagain
 
 board::board(int width, int height) : width(width), height(height), y(1), x(1)
 {
-    // init seed of rand() by current time
+    // init seed of rand() by current time and randomize turn point
     srand(time(NULL));
 
     this->lights = new bool*[height]();
@@ -18,11 +18,19 @@ board::board(int width, int height) : width(width), height(height), y(1), x(1)
         this->lights[i] = new bool[width]();
     }
 
-    // init and print the board
+    // init the board
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            if (rand() % 2 == 1){
+                turn(i, j, false);
+            }
+        }
+    }
+
+    // print the board
     print_board(width, height);
     for (int i = 0; i < height; i++){
         for (int j = 0; j < width; j++){
-            this->lights[i][j] = (rand() % 2 == 0);
             print_light(i, j, this->lights[i][j]);
         }
     }
@@ -62,22 +70,33 @@ void board::move_board(int dy, int dx)
     print_character(this->y, this->x, on ? 'o' : 'x', on ? ON : OFF);
 }
 
-void board::turn()
+void board::turn(int y, int x, bool print)
 {
-    this->lights[this->y][this->x] = !this->lights[this->y][this->x];
-    print_light(this->y, this->x, this->lights[this->y][this->x]);
+    this->lights[y][x] = !this->lights[y][x];
+    if (print){
+        print_light(y, x, this->lights[y][x]);
+    }
     for (int i = 0; i < 4; i++){
-        int ty = this->y + board::dy[i];
-        int tx = this->x + board::dx[i];
+        int ty = y + board::dy[i];
+        int tx = x + board::dx[i];
 
         if (0 <= ty && ty < this->height && 0 <= tx && tx < this->width){
             this->lights[ty][tx] = !this->lights[ty][tx];
-            print_light(ty, tx, this->lights[ty][tx]);
+            if (print){
+                print_light(ty, tx, this->lights[ty][tx]);
+            }
         }
     }
 
-    bool on = this->lights[this->y][this->x];
-    print_character(this->y, this->x, on ? 'o' : 'x', on ? ON : OFF);
+    if (print){
+        bool on = this->lights[y][x];
+        print_character(y, x, on ? 'o' : 'x', on ? ON : OFF);
+    }
+}
+
+void board::turn()
+{
+    this->turn(this->y, this->x, true);
 }
 
 const int board::dy[] = {-1, 0, 1, 0};
