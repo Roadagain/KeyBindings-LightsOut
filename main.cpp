3ad630f5570101f6f7aa077de622f29e319e7f4b
Roadagain <ringoh72@gmail.com>
2015-07-15 21:00:12 +0900

    Add getting keybindings from argv

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <ncurses.h>
+#include <cstring>
+
+#include <string>
 
 #include "print.hpp"
 #include "lightsout.hpp"
 #include "keybindings.hpp"
 using namespace roadagain;
 
-int main()
+int main(int argc, char** argv)
 {
     initscr();
     cbreak();
@@ -14,8 +17,15 @@ int main()
     start_color();
     init_colors();
 
+    std::string key = "vim";
+    for (int i = 1; i < argc; i++){
+        if (std::strncmp("--key=", argv[i], 6) == 0){
+            key = argv[i] + 6;
+        }
+    }
+
     board b(5, 5);
-    keybindings k("emacs");
+    keybindings k(key.c_str());
     while (!b.is_perfect()){
         char c = getch();
         const char* key = keyname(c);
